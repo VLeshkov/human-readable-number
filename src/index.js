505302bf020a5ae0a64098d5de2a5843e1f7a90c
Victor Leshkov <7065531+VLeshkov@users.noreply.github.com>
2021-08-14 21:42:55 +0600

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function toReadable (number) {
+    const numbers = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    const tenToNineteen = ['', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const dec = ['', 'ten', 'twenty ', 'thirty ', 'forty ', 'fifty ', 'sixty ', 'seventy ', 'eighty ', 'ninety '];
   
-}
+    let result = '';
+
+    if (number === 0) {
+        return 'zero';
+    }
+
+    if (number >= 100) {
+        result += `${numbers[String(number - number % 100) / 100]} hundred `;
+    }
+    
+    if (number % 100 <= 19 && number % 100 >= 10) {
+      result += `${tenToNineteen[number % 10 + 1]}`;
+    } else {
+      result += `${dec[(number % 100 - number % 10) / 10]}`;
+      result += `${numbers[number % 10]}`;
+    }
+  
+    return result.trim();
+  }
